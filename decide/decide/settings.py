6518fd5e3c68a63c9a5f12f621d7dc81320abb1a
jaime <jaime@mail.com>
2020-09-07 17:57:55 +0200

    fallo en census

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -164,7 +164,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'import django_heroku
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
--- decide/local_settings.py
+++ decide/local_settings.py
@@ -12,18 +12,26 @@ MODULES = [
     'visualizer',
     'voting',
 ]
-BASEAURL = 'http://127.0.0.1:5001'
+
+
+
+MODULES = [
+   
+    'census',
+]
+
+BASEAURL = 'http://localhost:8000'
 
 APIS = {
-    'authentication': BASEURL,
-    'base': BASEURL,
-    'booth': BASEURL,
-    'census': BASEURL,
-    'mixnet': BASEURL,
-    'postproc': BASEURL,
-    'store': BASEURL,
-    'visualizer': BASEURL,
-    'voting': BASEURL,
+    'authentication': 'http://localhost:8000',
+    'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
+    'mixnet': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
+    'visualizer': 'http://localhost:8000',
+    'voting': 'http://localhost:8000',
 }
 
 DATABASES = {
@@ -31,7 +39,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'decide',
         'USER': 'decide',
-        'HOST': 'db',
+	'USER': 'jailucloz',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
